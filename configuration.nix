134b47329d123796756f3015a5ab7018302c46e2
jchapuis <jonas.chapuis@nexthink.com>
2018-01-26 21:39:47 +0100

    reverted to simple user creation

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,19 @@
     options = "--delete-older-than 14d";
   };
 
+ # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.jchapuis = {
+     isNormalUser = true;
+     extraGroups = [
+      "audio" "disk" "docker" "networkmanager" "plugdev"
+      "systemd-journal" "wheel" "vboxusers" "video"
+      ];
+     uid = 1000;
+     keys = [
+       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHXTfECHVwgkguyvPB1Dn8cf3hgsUs4Q087YbtXL8ehXTZKBln6BhOPBO6FwVjRn/yDvyANpB9VRCOBbfOOtRYh12QoG4WYHtv+g9Y/r6vdu9LCkfUWUSFs9YRHLco92NDdj+AD2f4V+G502EkIOwAegtyj1u9QfaRnFf+QM7ytJAMeRDweWxsr6gfd8E67X/5EQtdnXGBBotovy5Hz65ku+2w8EyqgjfPs4iVEHkcaeXipBA/hFlh60/bmhHmRhh1w2LPRhKSP7Y7LdrMyxJQQtXsZNlfPhtYLzwMrm7n44oxe60+e1qUyHtbFjn0aysNijF+cIa4xWXBQ5ltJjV/ jonas.chapuis@nexthink.com"
+    ];
+   };
+
   # Enable manual on virtual console 8
   services.nixosManual.showManual = true;
 
