09113569d4ff4f5a324ca24e1440722c6ed477ee
jchapuis <jonas.chapuis@nexthink.com>
2018-01-26 21:44:48 +0100

    keys

--- configuration.nix
+++ configuration.nix
@@ -89,7 +89,7 @@
       "systemd-journal" "wheel" "vboxusers" "video"
       ];
      uid = 1000;
-     keys = [
+     openssh.authorizedKeys.keys = [
        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDHXTfECHVwgkguyvPB1Dn8cf3hgsUs4Q087YbtXL8ehXTZKBln6BhOPBO6FwVjRn/yDvyANpB9VRCOBbfOOtRYh12QoG4WYHtv+g9Y/r6vdu9LCkfUWUSFs9YRHLco92NDdj+AD2f4V+G502EkIOwAegtyj1u9QfaRnFf+QM7ytJAMeRDweWxsr6gfd8E67X/5EQtdnXGBBotovy5Hz65ku+2w8EyqgjfPs4iVEHkcaeXipBA/hFlh60/bmhHmRhh1w2LPRhKSP7Y7LdrMyxJQQtXsZNlfPhtYLzwMrm7n44oxe60+e1qUyHtbFjn0aysNijF+cIa4xWXBQ5ltJjV/ jonas.chapuis@nexthink.com"
     ];
    };
