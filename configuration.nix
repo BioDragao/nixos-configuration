3d62c47b2e9dd2cd8c696f872fbc6419d23c546c
jchapuis <jonas.chapuis@nexthink.com>
2018-01-26 22:17:40 +0100

    corrected timezone

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@
    };
 
   # Set your time zone.
-  time.timeZone = "Europe/Bern";
+  time.timeZone = "Europe/Paris";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
